3ab869a6830566d036e532e8e3c8e65b34b9bfe6
LoginDenied <32119742+LoginDenied@users.noreply.github.com>
2017-12-10 15:54:51 +0800

    Update Check_Prime.cpp

--- Check_Prime.cpp
+++ Check_Prime.cpp
@@ -25,3 +25,21 @@ int checkPrime(int number)
 
 	return isPrime;
 }
+
+int main()
+{
+	int numberToCheck;
+
+	cout << "Please enter the number to check" << endl;
+	cin >> numberToCheck;
+	if (checkPrime(numberToCheck) == 1)
+	{
+		cout << "The number entered is prime" << endl;
+	}
+	else
+	{
+		cout << "The number entered is not prime" << endl;
+	}
+
+    return 0;
+}
